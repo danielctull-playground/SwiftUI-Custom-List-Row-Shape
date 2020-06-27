da41aa0353c083ec6bfddf6cb25a0293fd9e46d9
Daniel Tull <dt@danieltull.co.uk>
2020-06-27 22:32:01 +0100

    Initial Commit

---
+++ Shared/Custom_List_Row_ShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_List_Row_ShapeApp.swift
+//  Shared
+//
+//  Created by Daniel Tull on 27.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct Custom_List_Row_ShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
